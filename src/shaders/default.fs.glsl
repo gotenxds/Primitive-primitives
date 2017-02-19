a66cc672554e128b6144fed3b236d707f8adcad1
gotenxds <david.limkys@gmail.com>
2017-02-19 11:55:07 +0200

    Fixed glsls

--- src/shaders/default.fs.glsl
+++ src/shaders/default.fs.glsl
@@ -1,9 +1,5 @@
-
-attribute vec3 position;
-attribute vec4 color;
 varying vec4 v_color;
-
-void main() {
-    gl_Position = czm_modelViewProjection * vec4(position, 1.0);
-    v_color = color;
+void main()
+{
+    gl_FragColor = v_color;
 }
--- src/shaders/default.vs.glsl
+++ src/shaders/default.vs.glsl
@@ -1,5 +1,8 @@
+attribute vec3 position;
+attribute vec4 color;
 varying vec4 v_color;
-void main()
-{
-    gl_FragColor = v_color;
+
+void main() {
+    gl_Position = czm_modelViewProjection * vec4(position, 1.0);
+    v_color = color;
 }
