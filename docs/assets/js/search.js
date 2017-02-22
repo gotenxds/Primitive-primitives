1b973df804741799e716185ecb033fc2d9836b38
gotenxds <david.limkys@gmail.com>
2017-02-22 13:32:53 +0200

    Allowing docs inside.

---
+++ docs/assets/js/search.js
@@ -0,0 +1,1 @@
+﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"main\"","url":"modules/_main_.html","classes":"tsd-kind-external-module"}]};
